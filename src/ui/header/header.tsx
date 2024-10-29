6caa4be4e0ec68cd8448399c153c7597b3a53a1d
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-29 15:24:31 -0400

    finish nav overlay on mobile

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -3,12 +3,12 @@ import logo from "../../../public/logo.svg";
 import cart from "../../../public/icons/icon-cart.svg";
 import styles from "./header.module.scss";
 import Link from "next/link";
-import NavLinks from "../nav-link/nav-link";
+import NavLinks from "../nav-links/nav-links";
 
 export default function Header(){
     return(
         <header className={`${styles["header"]}`}>
-            <div className={`content ${styles["header__content"]}`}>
+            <div className={`${styles["header__content"]}`}>
                 <NavLinks />
                 <Link href={"/"}>
                     <Image src={logo} alt="audiophile logo" height={25} width={143}/>
---
+++ src/ui/nav-links/nav-links.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import styles from "./nav-links.module.scss";
+import hamburger from "../../../public/icons/icon-hamburger.svg";
+import Image from "next/image";
+import Link from "next/link";
+import headphones from "../../../public/thumbnails/image-category-thumbnail-headphones.png";
+import earphones from "../../../public/thumbnails/image-category-thumbnail-earphones.png";
+import speakers from "../../../public/thumbnails/image-category-thumbnail-speakers.png";
+import { MouseEvent as ReactMouseEnvent, useEffect, useState } from "react";
+
+export default function NavLinks() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const header = document.querySelector("header");
+        const body = document.body;
+
+        const globalCloseHandler = (event: MouseEvent) => {
+            if(isOpen){
+                setIsOpen(false);
+            }
+        }
+
+        const escapeEvent = (event: KeyboardEvent) => {
+            if(event.key === "Escape"){
+                setIsOpen(false);
+            }
+        }
+
+        window.addEventListener("click", globalCloseHandler);
+        window.addEventListener("keyup", escapeEvent);
+
+        return () => {
+            window.removeEventListener("click", globalCloseHandler);
+            window.removeEventListener("keyup", escapeEvent);
+        }
+    });
+
+    const toggleHandler = (event: (ReactMouseEnvent)) => {
+        event.stopPropagation();
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <nav className={`${styles["nav"]}`} >
+            <button onClick={toggleHandler}>
+                <Image src={hamburger} alt="hamburger icon" height={15} width={16} />
+            </button>
+            <ul className={`${styles["nav__links"]} ${isOpen ? styles["nav__links--show"] : ""}`}>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/headphones"} className={`content ${styles["nav__link"]}`}>
+                        <Image src={headphones} alt="thumbnail for headphones category" className={`${styles["nav__image"]}`} height={104}/>
+                        HEADPHONES
+                        <span className={`${styles["nav__shop"]}`}>SHOP</span>
+                    </Link>
+                </li>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/speakers"} className={`content ${styles["nav__link"]}`}>
+                        <Image src={speakers} alt="thumbnail for speakers category" className={`${styles["nav__image"]}`} height={96}/>
+                        SPEAKERS
+                        <span className={`${styles["nav__shop"]}`}>SHOP</span>
+                    </Link>
+                </li>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/earphones"} className={`content ${styles["nav__link"]}`}>
+                    <Image src={earphones} alt="thumbnail for earphones category" className={`${styles["nav__image"]}`} height={96}/>
+                    EARPHONES
+                    <span className={`${styles["nav__shop"]}`}>SHOP</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
