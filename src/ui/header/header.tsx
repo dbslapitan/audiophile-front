94207167308c45de8904a7ec7325ec42f64684eb
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-29 05:10:14 -0400

    partial

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,17 +1,15 @@
 import Image from "next/image";
 import logo from "../../../public/logo.svg";
-import hamburger from "../../../public/icons/icon-hamburger.svg";
 import cart from "../../../public/icons/icon-cart.svg";
 import styles from "./header.module.scss";
 import Link from "next/link";
+import NavLinks from "../nav-link/nav-link";
 
 export default function Header(){
     return(
         <header className={`${styles["header"]}`}>
             <div className={`content ${styles["header__content"]}`}>
-                <button>
-                    <Image src={hamburger} alt="hamburger icon" height={15} width={16}/>
-                </button>
+                <NavLinks />
                 <Link href={"/"}>
                     <Image src={logo} alt="audiophile logo" height={25} width={143}/>
                 </Link>
---
+++ src/ui/nav-link/nav-link.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import styles from "./nav-link.module.scss";
+import hamburger from "../../../public/icons/icon-hamburger.svg";
+import Image from "next/image";
+import Link from "next/link";
+import headphones from "../../../public/thumbnails/image-category-thumbnail-headphones.png";
+import earphones from "../../../public/thumbnails/image-category-thumbnail-earphones.png";
+import speakers from "../../../public/thumbnails/image-category-thumbnail-speakers.png";
+
+export default function NavLinks() {
+    return (
+        <nav className={`${styles["nav"]}`}>
+            <button>
+                <Image src={hamburger} alt="hamburger icon" height={15} width={16} />
+            </button>
+            <ul className={`${styles["nav__links"]}`}>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/headphones"}>
+                        {/* <Image src={headphones} alt="thumbnail for headphones category" /> */}
+                    </Link>
+                </li>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/speakers"}>
+                        {/* <Image src={speakers} alt="thumbnail for speakers category" /> */}
+                        Headphones
+                    </Link>
+                </li>
+                <li className={`${styles["nav__item"]}`}>
+                    <Link href={"/earphones"}>
+                    {/* <Image src={earphones} alt="thumbnail for earphones category" /> */}
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
