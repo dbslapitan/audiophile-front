728d8448ffb3f7654ea2946405d8543849eeac88
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-29 04:22:18 -0400

    partial; on header

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   sassOptions: {
-    silenceDeprecation: ["legacy-js-api"]
+    silenceDeprecations: ["legacy-js-api"]
   }
 };
 
--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
 import logo from "../../../public/logo.svg";
+import hamburger from "../../../public/icons/icon-hamburger.svg";
+import cart from "../../../public/icons/icon-cart.svg";
 import styles from "./header.module.scss";
 import Link from "next/link";
 
 export default function Header(){
     return(
         <header className={`${styles["header"]}`}>
-            <Link href={"/"}>
-                <Image src={logo} alt="audiophile logo" height={25} width={143}/>
-            </Link>
+            <div className={`content ${styles["header__content"]}`}>
+                <button>
+                    <Image src={hamburger} alt="hamburger icon" height={15} width={16}/>
+                </button>
+                <Link href={"/"}>
+                    <Image src={logo} alt="audiophile logo" height={25} width={143}/>
+                </Link>
+                <Link href={"#"}>
+                    <Image src={cart} alt="cart icon" width={23} height={20}/>
+                </Link>
+            </div>
         </header>
     );
 }
