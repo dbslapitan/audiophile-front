606f376903a671c230b453fc49ada2233b30e997
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-10-31 13:16:35 -0400

    use picture element on hook component

--- src/ui/hook/hook.tsx
+++ src/ui/hook/hook.tsx
@@ -8,24 +8,12 @@ export default function Hook(){
 
     return(
         <section className={`content--fix ${styles["hook"]}`}>
-            <Image
-                src={mobile}
-                alt="a person touching the headphone he is wearing"
-                className={`${styles["hook__image"]} ${styles["hook__image--mobile"]}`}
-                height={300}
-            />
-            <Image
-                src={tablet}
-                alt="a person touching the headphone he is wearing"
-                className={`${styles["hook__image"]} ${styles["hook__image--tablet"]}`}
-                height={300}
-            />
-            <Image
-                src={desktop}
-                alt="a person touching the headphone he is wearing"
-                className={`${styles["hook__image"]} ${styles["hook__image--desktop"]}`}
-                height={588}
-            />
+            <picture className={`${styles["hook__image"]}`}>
+                <source srcSet={mobile.src} media="(max-width: 767px)"/>
+                <source srcSet={tablet.src} media="(min-width: 768px) and (max-width: 1233px)"/>
+                <source srcSet={desktop.src} media="(min-width: 1234px)"/>
+                <img src={mobile.src} alt="a person touching the headphone he is wearing" />
+            </picture>
             <div className={`${styles["hook__last"]}`}>
                 <h4 className={`${styles["hook__heading"]}`}>Bringing you the <span  className={`${styles["hook__heading--peach"]}`}>&nbsp;best&nbsp;</span> audio gear</h4>
                 <p className={`${styles["hook__description"]}`}>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
