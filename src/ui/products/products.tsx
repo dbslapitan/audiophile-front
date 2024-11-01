fc3bfc2bc4fbc452d1e2b6ba2e7db8cb36f56551
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-11-01 19:03:03 -0400

    finish design of category page on tablet

--- src/ui/products/products.tsx
+++ src/ui/products/products.tsx
@@ -11,11 +11,13 @@ export default function Products({data}: {data: Product[]}){
                         <li key={`${product.name}`} className={`${styles["product"]}`}>
                             <picture className={`${styles["product__picture"]}`}>
                                 <source srcSet={product.images.mobile.src} media={product.images.mobile.mediaQuery}/>
+                                <source srcSet={product.images.tablet.src} media={product.images.tablet.mediaQuery}/>
+                                <source srcSet={product.images.desktop.src} media={product.images.desktop.mediaQuery}/>
                                 <img className={`${styles["product__image"]}`} src={product.images.mobile.src} alt={product.alt} />
                             </picture>
                             <div className={`${styles["product__texts"]}`}>
                                 <h3 className={`${styles["product__new"]} ${product.isNew ? styles["product__new--show"] : ""}`}>NEW PRODUCT</h3>
-                                <h2 className={`${styles["product__name"]}`}>{`${product.name} ${product.category}`}</h2>
+                                <h2 className={`${styles["product__name"]}`}>{product.name} <span className={`${styles["product__category"]}`}>{product.category}</span></h2>
                                 <p className={`${styles["product__description"]}`}>{product.description}</p>
                                 <Link href={`/${product.category}/${product.urlSegment}`} className={`btn btn--peach ${styles["product__link"]}`}>SEE PRODUCT</Link>
                             </div>
